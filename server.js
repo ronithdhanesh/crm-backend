a98700bf3934aafb0cca959f9757ec3e10573873
ronithdhanesh <ronithdhanesh@gmail.com>
2025-09-10 14:07:39 +0530

    initial project setup and a simple homepage

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+app = express();
+port = 5002
+
+app.get("/", (req, res)=>{
+    res.send("hii");
+})
+app.listen(port, ()=>{
+    console.log("app running on port ",port)
+})
+
+
+
+console.log("server running ");
